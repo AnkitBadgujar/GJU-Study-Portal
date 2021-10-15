ea413798e0483909972d961457f8a8c2359d4dd8
ankit <ankitbadgujar78@gmail.com>
2021-10-15 17:18:29 +0530

    edit

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/',views.home,name='home' ),
+    path('',views.home,name='home' ),
 
     path('notes',views.notes,name='notes' ),
     path('delete_notes/<int:pk>',views.delete_notes,name='delete_notes' ),
